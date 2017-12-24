eb11d1d931d69486b311c3dbac23bfe8e7328c6a
Akababa <michael3795@gmail.com>
2017-12-23 23:56:50 -0600

    fork

--- src/chess_zero/run.py
+++ src/chess_zero/run.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import multiprocessing as mp
 
 _PATH_ = os.path.dirname(os.path.dirname(__file__))
 
@@ -10,7 +9,6 @@ if _PATH_ not in sys.path:
 
 
 if __name__ == "__main__":
-	mp.set_start_method('spawn')
 	sys.setrecursionlimit(10000)
 	from chess_zero import manager
 	manager.start()
